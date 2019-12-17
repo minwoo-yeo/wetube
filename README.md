e3b96bc42e0df4e9d8e721ded67974df177622d3
minwoo-yeo <fhghzl112@gmail.com>
2019-12-17 21:20:08 +0900

    Initial Commit

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # Wetube
 
 Cloning Youtube with Vanilla and NodeJS
+
+## Pages:
+
+- [] Home
+- [x] Join
+- [x] Login
+- [x] Search
+- [] user Detail
+- [x] edit Profile
+- [x] change Password
+- [x] Upload
+- [] Video Detail
+- [] Edit Video
+
---
+++ init.js
@@ -0,0 +1,15 @@
+import "./db";
+import dotenv from "dotenv";
+import app from "./app";
+
+dotenv.config();
+import "./models/Video";
+import "./models/Comment";
+import "./models/User";
+
+const PORT = process.env.PORT || 4000;
+
+const handleListening = () =>
+  console.log(`Listening on : http://localhost:${PORT}`);
+
+app.listen(PORT, handleListening);
